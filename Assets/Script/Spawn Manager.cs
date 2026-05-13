75f98e7002986a58597e5d56985659349c47385d
Deeapk Subramanian <deepakpavan0418@gmail.com>
2026-05-13 20:50:15 +0530

    Added Auto Spawn Function

--- Assets/Script/Spawn Manager.cs
+++ Assets/Script/Spawn Manager.cs
@@ -5,16 +5,23 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] AnimalPrefabs;
     private float SpawnRangeX = 20;
     private float SpawnRangeZ = 20;
+    private float SpawnDelay = 2;
+    private float SpawnInterval = 1.5f;
 
-    void Start() { }
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal",SpawnDelay, SpawnInterval);
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int randomIndex = Random.Range(0, AnimalPrefabs.Length);
-            Vector3 SpawnPosition =  new Vector3(Random.Range(-SpawnRangeX, SpawnRangeX), 0, SpawnRangeZ);
-            Instantiate(AnimalPrefabs[randomIndex], SpawnPosition, Quaternion.Euler(0, 180, 0));
-        }
+        
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int randomIndex = Random.Range(0, AnimalPrefabs.Length);
+        Vector3 SpawnPosition =  new Vector3(Random.Range(-SpawnRangeX, SpawnRangeX), 0, SpawnRangeZ);
+        Instantiate(AnimalPrefabs[randomIndex], SpawnPosition, Quaternion.Euler(0, 180, 0));
     }
 }
