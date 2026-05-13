30669f664e1065053b4a15e2a9c1f84af2c5cd1a
Deeapk Subramanian <deepakpavan0418@gmail.com>
2026-05-13 14:10:14 +0530

    Pizza-Shoot
    Added Projectile Pizza Prefab To the Player and Shoot when key is pressed Down

--- Assets/Script/Player Control.cs
+++ Assets/Script/Player Control.cs
@@ -6,6 +6,8 @@ public class PlayerControl : MonoBehaviour
     public float speed;
 
     public float xRange = 10.00f;
+
+    public GameObject ProjectilePrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -26,5 +28,10 @@ public class PlayerControl : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * (speed * horizontalInput * Time.deltaTime));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(ProjectilePrefab, transform.position, transform.rotation);
+        }
     }
 }
