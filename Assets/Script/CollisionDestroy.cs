e459b43a68081fb809ba057f16dee46f3c158917
Deeapk Subramanian <deepakpavan0418@gmail.com>
2026-05-14 14:15:49 +0530

    Added CollisonDestroy Script

---
+++ Assets/Script/CollisionDestroy.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+public class CollisionDestroy : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Pizza")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
